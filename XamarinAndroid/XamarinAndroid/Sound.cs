33f078509ce2f26c0af107368afa2cbafc771476
Ryouga1009 <mznryg@gmail.com>
2017-06-25 13:24:14 +0900

    AddProject

---
+++ XamarinAndroid/XamarinAndroid/Sound.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Media;
+using System.Collections.Generic;
+
+
+namespace XamarinAndroid
+{
+	public class Sound
+	{
+
+
+
+
+		public Sound(Context context)
+		{
+
+
+
+
+		}
+
+		public void loadSound(Context context,int rawId)
+		{
+
+
+		}
+
+
+
+		public void PlaySound(int rawId)
+		{
+			
+		}
+
+
+
+
+		public void release()
+		{
+			
+		}
+
+
+
+
+	}
+
+}
